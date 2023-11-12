5e5a84831429771becea1439374045be4044ec17
AstroSpiff <50888468+AstroSpiff@users.noreply.github.com>
2023-11-12 15:45:02 +0100

    Update trakt-watch-now-next.user.js

--- trakt-watch-now-next.user.js
+++ trakt-watch-now-next.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        Trakt.tv Watch Now Alternative
-// @namespace   https://github.com/sergeyhist/trakt-watch-now-alternative/blob/main/trakt-watch-now-next.user.js
+// @namespace   https://github.com/AstroSpiff/Trakt-ricerca-download-italiani/blob/main/trakt-watch-now-next.user.js
 // @match       *://trakt.tv/*
 // @version     4.6.3
 // @author      Hist
 // @description Alternative version for trakt.tv watch now modal
 // @run-at      document-start
-// @downloadURL https://github.com/sergeyhist/trakt-watch-now-alternative/raw/main/trakt-watch-now-next.user.js
-// @updateURL   https://github.com/sergeyhist/trakt-watch-now-alternative/raw/main/trakt-watch-now-next.user.js
-// @homepageURL https://github.com/sergeyhist/trakt-watch-now-alternative
+// @downloadURL https://github.com/AstroSpiff/Trakt-ricerca-download-italiani/raw/main/trakt-watch-now-next.user.js
+// @updateURL   https://github.com/AstroSpiff/Trakt-ricerca-download-italiani/raw/main/trakt-watch-now-next.user.js
+// @homepageURL https://github.com/AstroSpiff/Trakt-ricerca-download-italiani
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
